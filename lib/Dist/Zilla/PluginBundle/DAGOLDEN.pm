c4049a59e8df10f3babbd1290e2e0b868fe501e9
David Golden <dagolden@cpan.org>
2010-04-13 11:41:03 -0400

    temporary work

--- lib/Dist/Zilla/PluginBundle/DAGOLDEN.pm
+++ lib/Dist/Zilla/PluginBundle/DAGOLDEN.pm
@@ -1,16 +1,60 @@
 use strict;
 use warnings;
 package Dist::Zilla::PluginBundle::DAGOLDEN;
-# ABSTRACT: a module for CPAN
+# ABSTRACT: Dist::Zilla configuration the way DAGOLDEN does it
 
 # Dependencies
 use autodie 2.00;
 use Moose 0.99;
+use Moose::Autobox;
 use namespace::autoclean 0.09;
 
-# extends, roles, attributes, etc.
+use Dist::Zilla 2;
+with 'Dist::Zilla::Role::PluginBundle';
 
-# methods
+use Dist::Zilla::PluginBundle::Filter;
+use Dist::Zilla::PluginBundle::Git;
+
+sub bundle_config {
+  my ($self, $section) = @_;
+  my $class = (ref $self) || $self;
+
+  my $arg = $section->{payload};
+  my $is_task = $arg->{task};
+
+  my @plugins = Dist::Zilla::PluginBundle::Filter->bundle_config({
+      name    => $section->{name} . '/@Classic',
+      payload => {
+        bundle => '@Classic',
+        remove => [ qw(
+          PodVersion 
+          PodCoverageTests
+        ) ],
+      },
+    });
+
+  my $prefix = 'Dist::Zilla::Plugin::';
+  my @extra = map {[ "$section->{name}/$_->[0]" => "$prefix$_->[0]" => $_->[1] ]}
+  (
+    [ AutoPrereq  => {} ],
+    [ MetaConfig   => { } ],
+    [ MetaJSON     => { } ],
+    [ NextRelease  => { } ],
+    [ ($is_task ? 'TaskWeaver' : 'PodWeaver') => { config_plugin => '@RJBS' } ],
+    [ Repository   => { } ],
+  );
+
+  push @plugins, @extra;
+
+  push @plugins, Dist::Zilla::PluginBundle::Git->bundle_config({
+    name    => "$section->{name}/\@Git",
+    payload => {
+      tag_format => '%v',
+    },
+  });
+
+  return @plugins;
+}
 
 __PACKAGE__->meta->make_immutable;
 
@@ -19,7 +63,7 @@ __PACKAGE__->meta->make_immutable;
 __END__
 
 =for Pod::Coverage::TrustPod
-  method_name_here
+  bundle_config
 
 =begin wikidoc
 
