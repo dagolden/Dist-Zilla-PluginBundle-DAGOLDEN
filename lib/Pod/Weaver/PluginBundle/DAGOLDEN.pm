8e1dac5267aedc0afbb15e02b78c69954336e664
David Golden <dagolden@cpan.org>
2013-01-30 14:51:05 -0500

    more pod section construction

--- lib/Pod/Weaver/PluginBundle/DAGOLDEN.pm
+++ lib/Pod/Weaver/PluginBundle/DAGOLDEN.pm
@@ -1,5 +1,6 @@
 use strict;
 use warnings;
+
 package Pod::Weaver::PluginBundle::DAGOLDEN;
 # VERSION
 
@@ -9,9 +10,9 @@ use Pod::Weaver::Config::Assembler;
 # Dependencies
 use Pod::Weaver::Plugin::WikiDoc ();
 use Pod::Elemental::Transformer::List 0.101620 ();
-use Pod::Weaver::Section::Support 1.001 ();
+use Pod::Weaver::Section::Support 1.001        ();
 
-sub _exp { Pod::Weaver::Config::Assembler->expand_package($_[0]) }
+sub _exp { Pod::Weaver::Config::Assembler->expand_package( $_[0] ) }
 
 my $repo_intro = <<'END';
 This is open source software.  The code repository is available for
@@ -25,49 +26,55 @@ You will be notified automatically of any progress on your issue.
 END
 
 sub mvp_bundle_config {
-  my @plugins;
-  push @plugins, (
-    [ '@DAGOLDEN/WikiDoc',     _exp('-WikiDoc'), {} ],
-    [ '@DAGOLDEN/CorePrep',    _exp('@CorePrep'), {} ],
-    [ '@DAGOLDEN/Name',        _exp('Name'),      {} ],
-    [ '@DAGOLDEN/Version',     _exp('Version'),   {} ],
+    my @plugins;
+    push @plugins, (
+        [ '@DAGOLDEN/WikiDoc',  _exp('-WikiDoc'),  {} ],
+        [ '@DAGOLDEN/CorePrep', _exp('@CorePrep'), {} ],
+        [ '@DAGOLDEN/Name',     _exp('Name'),      {} ],
+        [ '@DAGOLDEN/Version',  _exp('Version'),   {} ],
 
-    [ '@DAGOLDEN/Prelude',     _exp('Region'),  { region_name => 'prelude'     } ],
-    [ '@DAGOLDEN/Synopsis',    _exp('Generic'), { header      => 'SYNOPSIS'    } ],
-    [ '@DAGOLDEN/Description', _exp('Generic'), { header      => 'DESCRIPTION' } ],
-    [ '@DAGOLDEN/Overview',    _exp('Generic'), { header      => 'OVERVIEW'    } ],
+        [ '@DAGOLDEN/Prelude',     _exp('Region'),  { region_name => 'prelude' } ],
+        [ '@DAGOLDEN/Synopsis',    _exp('Generic'), { header      => 'SYNOPSIS' } ],
+        [ '@DAGOLDEN/Description', _exp('Generic'), { header      => 'DESCRIPTION' } ],
+        [ '@DAGOLDEN/Overview',    _exp('Generic'), { header      => 'OVERVIEW' } ],
 
-    [ '@DAGOLDEN/Stability',   _exp('Generic'), { header      => 'STABILITY'   } ],
-  );
+        [ '@DAGOLDEN/Stability', _exp('Generic'), { header => 'STABILITY' } ],
+    );
 
-  for my $plugin (
-    [ 'Attributes', _exp('Collect'), { command => 'attr'   } ],
-    [ 'Methods',    _exp('Collect'), { command => 'method' } ],
-    [ 'Functions',  _exp('Collect'), { command => 'func'   } ],
-  ) {
-    $plugin->[2]{header} = uc $plugin->[0];
-    push @plugins, $plugin;
-  }
+    for my $plugin (
+        [ 'Usage',        _exp('Collect'), { command => 'usage' } ],
+        [ 'Functions',    _exp('Collect'), { command => 'func' } ],
+        [ 'Attributes',   _exp('Collect'), { command => 'attr' } ],
+        [ 'Constructors', _exp('Collect'), { command => 'construct' } ],
+        [ 'Methods',      _exp('Collect'), { command => 'method' } ],
+      )
+    {
+        $plugin->[2]{header} = uc $plugin->[0];
+        push @plugins, $plugin;
+    }
 
-  push @plugins, (
-    [ '@DAGOLDEN/Leftovers', _exp('Leftovers'), {} ],
-    [ '@DAGOLDEN/postlude',  _exp('Region'),    { region_name => 'postlude' } ],
-    [ '@DAGOLDEN/Support',   _exp('Support'),
-      {
-        perldoc => 0,
-        websites => 'none',
-        bugs => 'metadata',
-        bugs_content => $bugtracker_content,
-        repository_link => 'both',
-        repository_content => $repo_intro
-      }
-    ],
-    [ '@DAGOLDEN/Authors',   _exp('Authors'),   {} ],
-    [ '@DAGOLDEN/Legal',     _exp('Legal'),     {} ],
-    [ '@DAGOLDEN/List',      _exp('-Transformer'), { 'transformer' => 'List' } ],
-  );
+    push @plugins,
+      (
+        [ '@DAGOLDEN/Leftovers', _exp('Leftovers'), {} ],
+        [ '@DAGOLDEN/postlude', _exp('Region'), { region_name => 'postlude' } ],
+        [
+            '@DAGOLDEN/Support',
+            _exp('Support'),
+            {
+                perldoc            => 0,
+                websites           => 'none',
+                bugs               => 'metadata',
+                bugs_content       => $bugtracker_content,
+                repository_link    => 'both',
+                repository_content => $repo_intro
+            }
+        ],
+        [ '@DAGOLDEN/Authors', _exp('Authors'), {} ],
+        [ '@DAGOLDEN/Legal',   _exp('Legal'),   {} ],
+        [ '@DAGOLDEN/List', _exp('-Transformer'), { 'transformer' => 'List' } ],
+      );
 
-  return @plugins;
+    return @plugins;
 }
 
 # ABSTRACT: DAGOLDEN's default Pod::Weaver config
