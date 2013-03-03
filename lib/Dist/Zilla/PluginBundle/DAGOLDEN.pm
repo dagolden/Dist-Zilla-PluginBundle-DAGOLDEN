f83232cc1320072774a76ae5d59d9329671c0e76
David Golden <dagolden@cpan.org>
2013-03-02 23:06:01 -0500

    add Test::MinimumVersion

--- lib/Dist/Zilla/PluginBundle/DAGOLDEN.pm
+++ lib/Dist/Zilla/PluginBundle/DAGOLDEN.pm
@@ -32,6 +32,7 @@ use Dist::Zilla::Plugin::PodWeaver ();
 use Dist::Zilla::Plugin::ReadmeAnyFromPod 0.120051 ();
 use Dist::Zilla::Plugin::TaskWeaver 0.101620       ();
 use Dist::Zilla::Plugin::Test::Compile      ();
+use Dist::Zilla::Plugin::Test::MinimumVersion 2.000003 ();
 use Dist::Zilla::Plugin::Test::Perl::Critic ();
 use Dist::Zilla::Plugin::Test::PodSpelling 2.001002 ();
 use Test::Portability::Files 0.06 (); # buggy before that
@@ -211,6 +212,7 @@ sub configure {
 
         # generated t/ tests
         [ 'Test::Compile' => { fake_home => 1 } ],
+        [ 'Test::MinimumVersion' => { max_target_perl => '5.010' } ],
         'Test::ReportPrereqs',
 
         # generated xt/ tests
@@ -384,6 +386,9 @@ following dist.ini:
   ; t tests
   [Test::Compile]     ; make sure .pm files all compile
   fake_home = 1       ; fakes $ENV{HOME} just in case
+
+  [Test::MinimumPerl]
+  max_target_perl = 5.010
 
   [Test::ReportPrereqs] ; show prereqs in automated test output
 
