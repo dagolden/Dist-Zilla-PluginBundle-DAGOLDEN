5ae97846d2810de6aaaaf1d3d37cc5e87281a088
David Golden <dagolden@cpan.org>
2014-12-10 16:38:07 -0500

    After release: bump $VERSION and timestamp Changes

--- lib/Pod/Weaver/PluginBundle/DAGOLDEN.pm
+++ lib/Pod/Weaver/PluginBundle/DAGOLDEN.pm
@@ -3,7 +3,7 @@ use warnings;
 
 package Pod::Weaver::PluginBundle::DAGOLDEN;
 
-our $VERSION = '0.073';
+our $VERSION = '0.074';
 
 use Pod::Weaver 4; # he played knick-knack on my door
 use Pod::Weaver::Config::Assembler;
